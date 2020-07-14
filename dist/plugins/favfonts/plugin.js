5ffec2ab771046a679a962ecd3363e47f8e3dbd0
Andrei Popa <ndreipoppa@gmail.com>
2020-07-14 13:18:43 +0300

    “WIP”

--- dist/plugins/favfonts/plugin.js
+++ dist/plugins/favfonts/plugin.js
@@ -219,7 +219,7 @@
           var rebuildList = CKEDITOR.tools.bind(buildList, this);
           $(editor).bind('rebuildList', rebuildList);
           
-          this.add(null, 'null', null);
+          this.add('null', 'null', 'null');
 
           getUserFonts();
           // rebuildList();
