ea9f92c3dbb02ca406dbfb67dc48e5dd4fb7c4e9
Andrei Popa <ndreipoppa@gmail.com>
2020-07-15 16:45:06 +0300

    “WIP”

--- dist/plugins/favfonts/plugin.js
+++ dist/plugins/favfonts/plugin.js
@@ -81,6 +81,7 @@
       });
       allFonts = combinedFonts;
       $(editorInstance).trigger('rebuildList');
+      $('cke_combo__favfonts').addClass('loaded');
       // $(editorInstance).trigger('hideLoadingSpinner');
     }
 
