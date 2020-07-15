da094980440a3f06915357dbe7d5df41ebb7ebb6
Andrei Popa <ndreipoppa@gmail.com>
2020-07-15 13:24:11 +0300

    “WIP”

--- dist/plugins/BgColorKspectrum/plugin.js
+++ dist/plugins/BgColorKspectrum/plugin.js
@@ -1,44 +1,38 @@
-(function () {
-  'use strict';
+(function() {
+  function BackgroundColorBtn() {
+    var doc = new CKEDITOR.dom.window(window);
+    var jsVars = doc.$.parent.jsVars;
 
-  (function () {
-    function BackgroundColorBtn() {
-      var doc = new CKEDITOR.dom.window(window);
-      var jsVars = doc.$.parent.jsVars;
+    function addButton(editor) {
+      var config = editor.config;
 
-      function addButton(editor) {
-        var config = editor.config;
-        editor.addCommand("kspectrum_bg_color", {
-          exec: function exec(e) {
-            e.insertHtml('bgcolor');
-          }
-        });
-        editor.ui.addButton('BgColor', {
-          label: "Background Color",
-          command: 'kspectrum_bg_color',
-          toolbar: 'basicstyles',
-          icon: 'bgcolor'
-        });
-      }
-
-      function addPlugin() {
-        CKEDITOR.plugins.add('BgColorKspectrum', {
-          init: function init(editor) {
-            addButton(editor);
-          }
-        });
-      }
-
-      return {
-        init: function init() {
-          addPlugin();
+      editor.addCommand("kspectrum_bg_color", {
+        exec: function(e) {
+          e.insertHtml('bgcolor');
         }
-      };
+      });
+
+      editor.ui.addButton('BgColor', {
+        label: "Background Color",
+        command: 'kspectrum_bg_color',
+        toolbar: 'basicstyles',
+        icon: 'bgcolor'
+    });
     }
 
-    var plugin = new BackgroundColorBtn();
-    plugin.init();
-  })();
-
-}());
-//# sourceMappingURL=plugin.js.map
+    function addPlugin() {
+      CKEDITOR.plugins.add('BgColorKspectrum', {
+        init(editor) {
+          addButton(editor);
+        },
+      });
+    }
+    return {
+      init() {
+        addPlugin();
+      }
+    };
+  }
+  var plugin = new BackgroundColorBtn();
+  plugin.init();
+})();
