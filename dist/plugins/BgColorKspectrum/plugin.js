2f6af416d3445c8b0acf97e135552fa243c84a5b
Andrei Popa <ndreipoppa@gmail.com>
2020-07-15 13:17:12 +0300

    “WIP”

--- dist/plugins/BgColorKspectrum/plugin.js
+++ dist/plugins/BgColorKspectrum/plugin.js
@@ -1,2 +1,44 @@
-!function(){"use strict";(new function(){return new CKEDITOR.dom.window(window).$.parent.jsVars,{init:function(){CKEDITOR.plugins.add("BgColorKspectrum",{init:function(o){!function(o){o.config,o.addCommand("kspectrum_bg_color",{exec:function(o){o.insertHtml("bgcolor")}}),o.ui.addButton("BgColor",{label:"Background Color",command:"kspectrum_bg_color",toolbar:"basicstyles",icon:"bgcolor"})}(o)}})}}}).init()}();
+(function () {
+  'use strict';
+
+  (function () {
+    function BackgroundColorBtn() {
+      var doc = new CKEDITOR.dom.window(window);
+      var jsVars = doc.$.parent.jsVars;
+
+      function addButton(editor) {
+        var config = editor.config;
+        editor.addCommand("kspectrum_bg_color", {
+          exec: function exec(e) {
+            e.insertHtml('bgcolor');
+          }
+        });
+        editor.ui.addButton('BgColor', {
+          label: "Background Color",
+          command: 'kspectrum_bg_color',
+          toolbar: 'basicstyles',
+          icon: 'bgcolor'
+        });
+      }
+
+      function addPlugin() {
+        CKEDITOR.plugins.add('BgColorKspectrum', {
+          init: function init(editor) {
+            addButton(editor);
+          }
+        });
+      }
+
+      return {
+        init: function init() {
+          addPlugin();
+        }
+      };
+    }
+
+    var plugin = new BackgroundColorBtn();
+    plugin.init();
+  })();
+
+}());
 //# sourceMappingURL=plugin.js.map
--- dist/plugins/TextColorKspectrum/plugin.js
+++ dist/plugins/TextColorKspectrum/plugin.js
@@ -1,2 +1,44 @@
-!function(){"use strict";(new function(){return new CKEDITOR.dom.window(window).$.parent.jsVars,{init:function(){CKEDITOR.plugins.add("TextColorKspectrum",{init:function(t){!function(t){t.config,t.addCommand("kspectrum_text_color",{exec:function(t){t.insertHtml("textcolor")}}),t.ui.addButton("TextColor",{label:"Text Color",command:"kspectrum_text_color",toolbar:"basicstyles",icon:"textcolor"})}(t)}})}}}).init()}();
+(function () {
+  'use strict';
+
+  (function () {
+    function TextColorBtn() {
+      var doc = new CKEDITOR.dom.window(window);
+      var jsVars = doc.$.parent.jsVars;
+
+      function addButton(editor) {
+        var config = editor.config;
+        editor.addCommand("kspectrum_text_color", {
+          exec: function exec(e) {
+            e.insertHtml('textcolor');
+          }
+        });
+        editor.ui.addButton('TextColor', {
+          label: "Text Color",
+          command: 'kspectrum_text_color',
+          toolbar: 'basicstyles',
+          icon: 'textcolor'
+        });
+      }
+
+      function addPlugin() {
+        CKEDITOR.plugins.add('TextColorKspectrum', {
+          init: function init(editor) {
+            addButton(editor);
+          }
+        });
+      }
+
+      return {
+        init: function init() {
+          addPlugin();
+        }
+      };
+    }
+
+    var plugin = new TextColorBtn();
+    plugin.init();
+  })();
+
+}());
 //# sourceMappingURL=plugin.js.map
