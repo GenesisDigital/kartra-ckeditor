cde53b01c476badafeba96118b66bc542055fdf4
Andrei Popa <ndreipoppa@gmail.com>
2020-07-16 11:07:34 +0300

    “WIP”

--- dist/plugins/favfonts/plugin.js
+++ dist/plugins/favfonts/plugin.js
@@ -81,7 +81,8 @@
       });
       allFonts = combinedFonts;
       $(editorInstance).trigger('rebuildList');
-      $('.cke_combo__favfonts').addClass('loaded');
+      $('.cke_combo__favfonts').addClass('loaded'); // cke_combo_text
+      $('.cke_combo__favfonts .cke_combo_text')[0].innerText = $('.cke_combo__favfonts .cke_combo_text')[0].innerText.replace(/"/g,'');
     }
 
     function buildList() {
