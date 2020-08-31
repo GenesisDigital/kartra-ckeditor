95bebb306a4bddd8014f0e7f3babe9382640e9df
Andrei Popa <ndreipoppa@gmail.com>
2020-08-31 16:24:06 +0300

    “WIP”

--- dist/plugins/favfonts/plugin.js
+++ dist/plugins/favfonts/plugin.js
@@ -1,33 +1,16 @@
-(function() {
-  function FavFonts() {
-    function addButton(editor) {
-      editor.addCommand( 'insertTimestamp', {
-        exec: function( editor ) {
-            var now = new Date();
-            editor.insertHtml( 'The current date and time is: <em>' + now.toString() + '</em>' );
-        }
-      });
-      editor.ui.addButton( 'Timestamp', {
-          label: 'Insert Timestamp',
-          command: 'insertTimestamp',
-          toolbar: 'insert'
-      });
-    }
-
-    function addPlugin() {
-      CKEDITOR.plugins.add('favfonts', {
-        icons: 'timestamp',
-        init(editor) {
-          addButton(editor);
-        },
-      });
-    }
-    return {
-      init() {
-        addPlugin();
+CKEDITOR.plugins.add('favfonts', {
+  icons: 'timestamp',
+  init(editor) {
+    editor.addCommand( 'insertTimestamp', {
+      exec: function( editor ) {
+          var now = new Date();
+          editor.insertHtml( 'The current date and time is: <em>' + now.toString() + '</em>' );
       }
-    };
-  }
-  var plugin = new FavFonts();
-  plugin.init();
-})();
+    });
+    editor.ui.addButton( 'Timestamp', {
+        label: 'Insert Timestamp',
+        command: 'insertTimestamp',
+        toolbar: 'insert'
+    });
+  },
+}
